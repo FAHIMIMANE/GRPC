a1e8f16d32eb058bd2bda1365512f643d23865b9
FAHIM IMANE <imanefahim20@gmail.com>
2024-12-07 17:17:35 +0100

    first commit

---
+++ src/main/java/ma/projet/grpc/repositories/CompteRepository.java
@@ -0,0 +1,9 @@
+package ma.projet.grpc.repositories;
+
+
+
+import ma.projet.grpc.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompteRepository extends JpaRepository<Compte,String> {
+}
